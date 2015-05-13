c7604b16d7415ab5e1383fc143c0923be5bbdd37
Stephan Bönnemann <stephan@boennemann.me>
2015-05-14 01:30:02 +0200

    feat(sum): add support for stringified numbers

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = function () {
   return Array.prototype.slice.call(arguments)
 
   .reduce(function (prev, current) {
-    return prev + current
-  })
+    return prev + Number(current)
+  }, 0)
 }
--- tests/basics.js
+++ tests/basics.js
@@ -3,8 +3,9 @@ var test = require('tape')
 var sum = require('../')
 
 test('basics', function (t) {
-  t.plan(2)
+  t.plan(3)
 
   t.is(sum(1, 4), 5)
   t.is(sum(1), 1)
+  t.is(sum(1, '4'), 5)
 })
