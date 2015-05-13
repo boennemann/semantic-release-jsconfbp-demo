0eb0f6aa4ee9305e43a0aa0e8d5845a4c7c6a3c5
Stephan Bönnemann <stephan@boennemann.me>
2015-05-14 01:40:05 +0200

    feat(sum): throws error for less than two arguments
    BREAKING CHANGE: No more support for single argument. Use at least two.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = function () {
+  if (arguments.length < 2) throw new Error('At least two arguments please!')
+
   return Array.prototype.slice.call(arguments)
 
   .reduce(function (prev, current) {
--- tests/basics.js
+++ tests/basics.js
@@ -5,7 +5,7 @@ var sum = require('../')
 test('basics', function (t) {
   t.plan(3)
 
+  t.throws(sum.bind(null, 1))
   t.is(sum(1, 4), 5)
-  t.is(sum(1), 1)
   t.is(sum(1, '4'), 5)
 })
